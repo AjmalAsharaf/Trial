cbe26f501b59a3e9febc3ad16a59e43eba315071
AjmalAsharaf <ajmalash72@gmail.com>
2021-03-25 18:48:03 +0530

    final

---
+++ assets/js/script.js
@@ -0,0 +1,57 @@
+function test() {
+    document.getElementById('imgplus').style.display = 'none'
+    document.getElementById('imgminus').style.display = 'block'
+
+}
+
+function testminus() {
+    document.getElementById('imgminus').style.display = 'none'
+    document.getElementById('imgplus').style.display = 'block'
+
+}
+function testone() {
+    document.getElementById('imgplusone').style.display = 'none'
+    document.getElementById('imgminusone').style.display = 'block'
+    
+    
+
+}
+
+function testminusone() {
+    document.getElementById('imgminusone').style.display = 'none'
+    document.getElementById('imgplusone').style.display = 'block'
+
+}
+function testtwo() {
+    document.getElementById('imgplustwo').style.display = 'none'
+    document.getElementById('imgminustwo').style.display = 'block'
+
+}
+
+function testminustwo() {
+    document.getElementById('imgminustwo').style.display = 'none'
+    document.getElementById('imgplustwo').style.display = 'block'
+
+}
+function testthree() {
+    document.getElementById('imgplusthree').style.display = 'none'
+    document.getElementById('imgminusthree').style.display = 'block'
+
+}
+
+function testminusthree() {
+    document.getElementById('imgminusthree').style.display = 'none'
+    document.getElementById('imgplusthree').style.display = 'block'
+
+}
+function testfour() {
+    document.getElementById('imgplusfour').style.display = 'none'
+    document.getElementById('imgminusfour').style.display = 'block'
+
+}
+
+function testminusfour() {
+    document.getElementById('imgminusfour').style.display = 'none'
+    document.getElementById('imgplusfour').style.display = 'block'
+
+}
