eb51548e5965cda6c7e03b8fc8e4bd863eea42cf
AjmalAsharaf <ajmalash72@gmail.com>
2021-03-27 13:10:24 +0530

    submision form added

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,3 +55,77 @@ function testminusfour() {
     document.getElementById('imgplusfour').style.display = 'block'
 
 }
+
+//form validation
+
+$('#contactform').validate({
+    rules:{
+        fname:{
+            required:true,
+            minlength:4,
+            
+        },
+        number:{
+            required:true,
+            minlength:10,
+            maxlength:10,
+            number:true
+            
+        },
+        email:{
+            required:true,
+            minlength:5,
+            email:true
+        },
+        link:{
+            required:true,
+            url:true
+        },
+        aboutyou:{
+            required:true,
+            minlength:140,
+            maxlength:1000
+        }
+
+    },
+    messages:{
+        fname:{
+            required:'Name is required'
+        },
+        number:{
+            required:'Contact Number is required',
+
+        },
+        email:{
+            required:'Email is required'
+        },
+        link:{
+            required:'Social media link is required'
+        },
+        aboutyou:{
+            required:'Tell us more about you'
+        }
+
+    },submitHandler:function(contactform){
+        $('#sub-button').hide()
+        $.ajax({
+            url:contactform.action,
+            method:"post",
+            data:$('#contactform').serialize(),
+            success:function(response){
+                $('#send-confirm-text').show()
+                setTimeout(function(){location.reload()},3000)
+                
+                
+            },
+            error:function (err)
+              {
+          alert("Something went wrong")
+              }
+
+        })
+
+    }
+
+
+})
